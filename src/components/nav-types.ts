d2251993b828d0824519d1561ad546d238cd0830
Sven Read <me@svenread.com>
2024-09-24 23:16:39 +0200

    reverted fix

--- src/components/nav-types.ts
+++ src/components/nav-types.ts
@@ -3,7 +3,7 @@ import { LucideIcon } from "lucide-react"
 export interface NavItem {
   title: string
   url: string
-  icon: React.ElementType 
+  icon?: LucideIcon
   isActive?: boolean
   items?: NavItem[]
 }
