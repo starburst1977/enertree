293d1a80bef48fe26f7e3688c5d37bed4ac95548
Sven Read <me@svenread.com>
2024-09-26 15:23:52 +0200

    Add button to rack

--- src/components/dashboard-02.tsx
+++ src/components/dashboard-02.tsx
@@ -31,6 +31,10 @@ import {
 import {
   SidebarTrigger,
 } from "@/components/ui/sidebar"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faServer as farServer } from '@fortawesome/pro-regular-svg-icons'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 
 export default function Dashboard() {
@@ -40,25 +44,27 @@ export default function Dashboard() {
       
       <div className="flex flex-col sm:gap-4 sm:py-4">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-white px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 dark:bg-neutral-950">
-         
+        
           <Breadcrumb className="hidden md:flex">
             <SidebarTrigger />
             <BreadcrumbList>
               <BreadcrumbItem>
                 <BreadcrumbLink asChild>
                   <Link href="#" className="text-blue-700 font-bold text-lg">Dashboard Rack</Link>
-                  
                 </BreadcrumbLink>
               </BreadcrumbItem>
               
             </BreadcrumbList>
           </Breadcrumb>
+          <Button variant="outline" size="default" className="flex items-center gap-2">
+          <FontAwesomeIcon icon={farServer} /> Show all devices in rack
+          </Button>
           <div className="relative ml-auto flex-1 md:grow-0">
-
+            
             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-neutral-500 dark:text-neutral-400" />
             <Input
               type="search"
-              placeholder="Search..."
+              placeholder="Search......."
               className="w-full rounded-lg bg-white pl-8 md:w-[200px] lg:w-[320px] dark:bg-neutral-950"
             />
           </div>
