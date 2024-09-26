a9abd6de21b105e2466d6a09307b17c18353679f
Sven Read <me@svenread.com>
2024-09-26 15:27:01 +0200

    fix

--- src/components/dashboard-04.tsx
+++ src/components/dashboard-04.tsx
@@ -33,7 +33,6 @@ import {
 } from "@/components/ui/sidebar"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faServer as farServer } from '@fortawesome/pro-regular-svg-icons'
-import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
 
