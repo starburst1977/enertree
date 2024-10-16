6ea35d66e6491e7628394af1c1bd5dc1218b92f1
Sven Read <me@svenread.com>
2024-10-16 23:13:35 +0200

    Another one

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 "use client"
 
 import * as React from "react"
@@ -363,3 +365,5 @@ export {
   ChartLegendContent,
   ChartStyle,
 }
+
+/* eslint-enable @typescript-eslint/no-unused-vars */
