ba6353dd63d470aadede80aef67ba178c35b5f04
Sven Read <me@svenread.com>
2025-01-13 00:24:17 +0100

    Spinner

--- src/components/device-view.tsx
+++ src/components/device-view.tsx
@@ -4,6 +4,7 @@
 
 import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 
+
 import {
   Drawer,
   DrawerClose,
@@ -174,6 +175,30 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSquareBolt as faSquareBolt, faTriangle as faTriangle, faCircle as faCircle, faXmark as faXmark, faArrowsDownToLine as faArrowsDownToLine, faArrowUpToLine as faArrowUpToLine, faArrowDownToLine as faArrowDownToLine } from '@fortawesome/pro-solid-svg-icons'
 import { faArrowUpRightFromSquare as faArrowUpRightFromSquare, faPowerOff as faPowerOff, } from '@fortawesome/pro-regular-svg-icons'
+
+const LoadingSpinner = () => {
+  return (
+    <div role="status">
+      <svg aria-hidden="true" class="w-8 h-8 text-blue-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <span class="sr-only">Loading...</span>
+  </div>
+  );
+};
+
+const LoadingSpinnerSmall = () => {
+  return (
+    <div role="status">
+      <svg aria-hidden="true" class="w-4 h-4 text-blue-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <span class="sr-only">Loading...</span>
+  </div>
+  );
+};
 
 export function DialogComponent() {
   const [activeChart, setActiveChart] =
@@ -375,7 +400,7 @@ export function DialogComponent() {
               Create Threshold
             </span>
             <Input type="text" className="w-36" placeholder="Name" disabled />
-            <Button variant="primary" className="w-24 bg-gray-100 text-gray-300">Save</Button>
+            <Button variant="primary" className="w-24 bg-gray-100 text-gray-300 flex items-center gap-2"><LoadingSpinnerSmall /> Save</Button>
           </div>
 
       </CardFooter>
@@ -734,7 +759,8 @@ export default function DeviceViewComponent() {
                       </Table>
                     </CardContent>
                   </Card>
-                  <Card className="max-w-3xl">
+                  <Card className="max-w-3xl relative">
+                    <div className="absolute top-1 right-1 bottom-1 left-1 bg-white/90 flex items-center justify-center z-10 rounded-xl"><LoadingSpinner /></div>
                     <CardHeader className="p-6 pb-6">
                       <CardTitle>Environment Sensors</CardTitle>
                     </CardHeader>
