f985cc6597ac9569166ed4396428d70bfd9aaec9
Sven Read <me@svenread.com>
2025-01-15 23:12:42 +0100

    form update

--- src/components/device-view.tsx
+++ src/components/device-view.tsx
@@ -188,6 +188,18 @@ const LoadingSpinner = () => {
   );
 };
 
+const LoadingSpinnerLarge = () => {
+  return (
+    <div role="status">
+      <svg aria-hidden="true" className="w-12 h-12 text-blue-200 animate-spin fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+      </svg>
+      <span className="sr-only">Loading...</span>
+  </div>
+  );
+};
+
 const LoadingSpinnerSmall = () => {
   return (
     <div role="status">
@@ -974,49 +986,114 @@ export default function DeviceViewComponent() {
               </div>
             </TopTabsContent>
             <TopTabsContent value="config">Configuration</TopTabsContent>
-            <TopTabsContent value="notifications">Notifications</TopTabsContent>
+            <TopTabsContent value="notifications" className="relative h-96">
+              <div className="absolute top-1 right-1 bottom-1 left-1 flex items-center justify-center z-10 rounded-xl"><LoadingSpinnerLarge /></div>
+            </TopTabsContent>
             <TopTabsContent value="backups">
-              <Card className="max-w-xl">
-                <CardHeader>
-                  <CardTitle>Form Examples</CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="flex flex-col gap-4 p-4">
-                    <div className="flex items-center gap-4 relative">
-                      <Label>Name</Label>
-                      <Input className="bg-gray-50 border-gray-300"/>
-                      
+              <div className="flex gap-4">
+                <Card className="w-full">
+                  <CardHeader>
+                    <CardTitle>Form Examples</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                        <Label>Name</Label>
+                        <Input className="bg-gray-50 border-gray-300"/>
+                        
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex flex-col gap-4 p-4">
-                    <div className="flex items-center gap-4 relative">
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                        <Label className="flex items-center gap-2">Name <span className="text-red-600 font-bold">*</span></Label>
+                        <Input className="bg-gray-50 border-gray-300"/>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                        <Label>Name</Label>
+                        <Input className="bg-gray-50 border-red-700" value="test"/>
+                        <div className="absolute bottom-full left-1/2 z-20 mb-0.5 -translate-x-1/2 whitespace-nowrap rounded-md bg-red-700 py-1 px-2 text-xs text-white font-medium " role="tooltip" id="top-tooltip">
+                          <span className="absolute -bottom-1 left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 border-b border-r border-white bg-red-700"></span>
+                          Please enter a valid name
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
                       <Label className="flex items-center gap-2">Name <span className="text-red-600 font-bold">*</span></Label>
-                      <Input className="bg-gray-50 border-gray-300"/>
+                        <Input  className="bg-gray-50 border-red-600" />
+                        <div className="absolute bottom-full left-1/2 z-20 mb-0.5 -translate-x-1/2 whitespace-nowrap rounded-md bg-red-600 py-1 px-2 text-xs text-white font-medium " role="tooltip" id="top-tooltip">
+                          <span className="absolute -bottom-1 left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 border-b border-r border-white bg-red-600"></span>
+                          Missing required field
+                        </div>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card className="w-full">
+                  <CardHeader>
+                    <CardTitle>Button Examples</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                        <Label>Standard</Label>
+                        <Button variant="default">Button</Button>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                        <Label>Secondary</Label>
+                        <Button variant="secondary">Button</Button>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                      <Label>Delete</Label>
+                      <Button variant="destructive">Button</Button>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                        <Label>Outline</Label>
+                        <Button variant="outline">Button</Button>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                      <Label>Disabled</Label>
+                      <Button variant="disabled">Button</Button>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                      <Label>Loading</Label>
+                      <Button variant="disabled" className="flex items-center gap-2"><LoadingSpinnerSmall /> Loading</Button>
+                        
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <div className="flex items-center gap-4 relative">
+                      <Label>Form Error</Label>
+                      <Button variant="error" className="relative">Button
+                      <div className="absolute bottom-full left-1/2 z-20 mb-0.5 -translate-x-1/2 whitespace-nowrap rounded-md bg-orange-600 py-1 px-2 text-xs text-white font-medium " role="tooltip" id="top-tooltip">
+                        <span className="absolute -bottom-1 left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 border-b border-r border-white bg-orange-600"></span>
+                        Form Error
+                      </div>
+                      </Button>
                       
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-4 p-4">
-                    <div className="flex items-center gap-4 relative">
-                      <Label>Name</Label>
-                      <Input className="bg-gray-50 border-red-600" value="test"/>
-                      <div className="absolute bottom-full left-1/2 z-20 mb-0.5 -translate-x-1/2 whitespace-nowrap rounded-md bg-red-600 py-1 px-2 text-xs text-white font-medium " role="tooltip" id="top-tooltip">
-                        <span className="absolute -bottom-1 left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 border-b border-r border-white bg-red-600"></span>
-                        Please enter a valid name
                       </div>
                     </div>
-                  </div>
-                  <div className="flex flex-col gap-4 p-4">
-                    <div className="flex items-center gap-4 relative">
-                      <Label>Name</Label>
-                      <Input  className="bg-gray-50 border-red-600" />
-                      <div className="absolute bottom-full left-1/2 z-20 mb-0.5 -translate-x-1/2 whitespace-nowrap rounded-md bg-red-600 py-1 px-2 text-xs text-white font-medium " role="tooltip" id="top-tooltip">
-                        <span className="absolute -bottom-1 left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45 border-b border-r border-white bg-red-600"></span>
-                        Missing required field
-                      </div>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
+                  </CardContent>
+                </Card>
+              </div>
             </TopTabsContent>
           </TopTabs>
           </main>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,14 +10,16 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-blue-600 text-white hover:bg-neutral-900/90 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-neutral-50/90",
+          "bg-blue-600 text-white hover:bg-blue-500",
         destructive:
           "bg-red-500 text-neutral-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90",
         outline:
-          "border border-neutral-200 bg-white hover:bg-neutral-100 hover:text-neutral-900 dark:border-neutral-800 dark:bg-neutral-950 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
+          "border border-neutral-200 hover:border-blue-200 bg-white hover:bg-blue-100 hover:text-neutral-900",
         secondary:
-          "bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80",
-        ghost: "hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
+          "bg-neutral-100 text-neutral-900 hover:bg-blue-100",
+        ghost: "bg-gray-100 hover:bg-blue-100 hover:text-neutral-900",
+        disabled: "bg-gray-100 text-gray-300 cursor-not-allowed",
+        error: "bg-gray-100 text-red-600 cursor-not-allowed",
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50",
       },
       size: {
